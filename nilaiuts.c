8bc7de80146f19740a0042632048e74ec21407c2
fathanfatazka <fathan_fatazka@apps.ipb.ac.id>
2020-04-04 10:03:55 +0700

    Membuat file c untuk me-list nilai uts sebelum liburan

---
+++ nilaiuts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Sebelum liburan alangkah baiknya kita mendata nilai uts terlebih dahulu.\n");
+    printf("Berikut adalah nilai hasil uts Pateng: \n");
+    return 0;
+}
