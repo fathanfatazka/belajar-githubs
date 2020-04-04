8fb5805005b241a35f3a4b963b616d41b1f09219
fathanfatazka <fathan_fatazka@apps.ipb.ac.id>
2020-04-04 10:28:30 +0700

    Gatau ini commit apaan

--- nilaiuts.c
+++ nilaiuts.c
@@ -3,5 +3,11 @@
 int main(void){
     printf("Sebelum liburan alangkah baiknya kita mendata nilai uts terlebih dahulu.\n");
     printf("Berikut adalah nilai hasil uts Pateng: \n");
+<<<<<<< HEAD
+=======
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    printf("%d\n%d\n%d", a, b, c);
+>>>>>>> 55744a4... Menambah file nilai uts dan menambah list rencana rekreasi
     return 0;
 }
--- rencana.c
+++ rencana.c
@@ -3,5 +3,6 @@
 int main(void){
     printf("Berikut adalah kumpulan file target rekreasi pateng di 2020, semoga tercapai~\n");
     printf("1. Mau ke Malaysia\n");
+    printf("2. Aku mau ke new zealand\n");
     return 0;
 }
